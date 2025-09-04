3b7c8306476aacc0ab9ebed471d9b701155f8208
Zhozemir <joji013@abv.bg>
2025-09-04 13:03:47 +0300

    add models

--- server/src/main/java/com/bank/models/Customer.java
+++ server/src/main/java/com/bank/models/Customer.java
@@ -1,8 +1,71 @@
 package com.bank.models;
 
+import java.math.BigDecimal;
+
 public class Customer {
 
     private String username;
+    private String password;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private BigDecimal balance;
+
+    public Customer(String username, String password, String firstName, String lastName, String email) {
+
+        setUsername(username);
+        setPassword(password);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmail(email);
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
 
 }
 
---
+++ server/src/main/java/com/bank/models/Employee.java
@@ -0,0 +1,7 @@
+package com.bank.models;
+
+public class Employee {
+
+
+
+}
