c2f61bb206c1031adfd7286ad3c311d3e73d77f4
NikolayFrunze <niki.frunze@outlook.com>
2025-09-04 12:40:40 +0300

    proper connection to DB

--- server/src/main/java/com/bank/ServerApplication.java
+++ server/src/main/java/com/bank/ServerApplication.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(
-    exclude = {
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
-    }
+
 )
 public class ServerApplication {
     public static void main(String[] args) {
