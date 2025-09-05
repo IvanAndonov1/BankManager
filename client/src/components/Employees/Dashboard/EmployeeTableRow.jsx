4f8fc0c0753cfd7b1a2babd02e79302694a3f5f4
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-05 17:08:22 +0300

    employee table row component

---
+++ client/src/components/Employees/Dashboard/EmployeeTableRow.jsx
@@ -0,0 +1,16 @@
+function EmployeeTableRow(props) {
+	return (
+		<tr className="hover:bg-gray-50">
+			<td className="py-3">{props.name}</td>
+			<td className="py-3">{props.date}</td>
+			<td className="py-3 text-green-600">{props.status}</td>
+			<td className="py-3">
+				<button className="cursor-pointer px-4 py-1 rounded-full text-white" style={{ backgroundColor: "#351f78" }}>
+					Show more
+				</button>
+			</td>
+		</tr>
+	);
+}
+
+export default EmployeeTableRow;
