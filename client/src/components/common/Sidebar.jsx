93819d57a2ea4876323c2cbcf4fecdc4af2eca3e
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-05 17:07:56 +0300

    employee page sidebar

---
+++ client/src/components/common/Sidebar.jsx
@@ -0,0 +1,12 @@
+function Sidebar() {
+	return (
+		<div
+			className="w-16 rounded-r-2xl"
+			style={{
+				background: "linear-gradient(to bottom, #351F78 0%, #0B82BE 100%)",
+			}}
+		/>
+	);
+}
+
+export default Sidebar;
