009140cbfcd38e4b7556edca301629d04c160496
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-05 17:07:22 +0300

    setup react routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,13 @@
+import { Routes, Route } from 'react-router';
+import Dashboard from './components/Employees/Dashboard/Dashboard';
+
 function App() {
 	return (
-		<></>
+		<>
+			<Routes>
+				<Route path="/employee" element={<Dashboard />} />
+			</Routes>
+		</>
 	)
 }
 
