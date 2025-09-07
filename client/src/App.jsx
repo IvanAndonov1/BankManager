ffac7468739082526e069a1e81c824e8b6ec7b98
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-08 01:16:17 +0300

    add new route for customer details page in employee tab

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router';
 import Dashboard from './components/Employees/Dashboard/Dashboard';
+import CustomerMoreInfo from './components/Employees/Customer/Details/Details';
+
 
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route path="/employee" element={<Dashboard />} />
+				<Route path="/more-info" element={<CustomerMoreInfo />} />
 			</Routes>
 		</>
 	)
