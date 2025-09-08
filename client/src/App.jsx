6642378b0bfd81b088d2d8b66ae474f8381c1885
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-09 01:19:38 +0300

    add new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router';
 import Dashboard from './components/Employees/Dashboard/Dashboard';
 import CustomerMoreInfo from './components/Employees/Customer/Details/Details';
+import Home from './components/Home/Home';
+import Login from './components/Auth/Login/Login';
 
 
 function App() {
 	return (
 		<>
 			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
 				<Route path="/employee" element={<Dashboard />} />
 				<Route path="/customer-details/:employeeId" element={<CustomerMoreInfo />} />
 			</Routes>
