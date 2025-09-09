2990fef588a64b16f2fbaeb3c661f31dc3599319
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-09 13:47:11 +0300

    add new routes to the application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import Dashboard from './components/Employees/Dashboard/Dashboard';
 import CustomerMoreInfo from './components/Employees/Customer/Details/Details';
 import Home from './components/Home/Home';
 import Login from './components/Auth/Login/Login';
+import AdminDashboard from './components/Admin/Dashboard/Dashboard';
+import AdminEmployeeDetails from './components/Admin/Employee/Details/AdminEmployeeDetails';
 
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
 				<Route path="/login" element={<Login />} />
 				<Route path="/employee" element={<Dashboard />} />
 				<Route path="/customer-details/:employeeId" element={<CustomerMoreInfo />} />
+				<Route path="/admin" element={<AdminDashboard />} />
+				<Route path="/admin/employee/:employeeId" element={<AdminEmployeeDetails />} />
 			</Routes>
 		</>
 	)
