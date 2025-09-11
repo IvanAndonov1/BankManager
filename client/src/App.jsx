9654c1ec091b7c6b54a9bdc0fa8578ca203ef31a
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-11 14:06:45 +0300

    add new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import Login from './components/Auth/Login/Login';
 import AdminDashboard from './components/Admin/Dashboard/Dashboard';
 import AdminEmployeeDetails from './components/Admin/Employee/Details/AdminEmployeeDetails';
+import Register from './components/Auth/Register/Register';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
 				<Route path="/employee" element={<Dashboard />} />
 				<Route path="/customer-details/:employeeId" element={<CustomerMoreInfo />} />
 				<Route path="/admin" element={<AdminDashboard />} />
