70bdba949f3107bf99d64fb6b4eef7f9f1bf3f08
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-16 16:04:01 +0300

    user service api function

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -4,4 +4,4 @@ const baseUrl = 'http://localhost:8080/api';
 
 export const loginUser = (data) => requester.post(`${baseUrl}/auth/login`, data);
 
-export const registerUser = (data) => requester.post(`${baseUrl}/auth/reigster`, data);
+export const registerUser = (data) => requester.post(`${baseUrl}/auth/register`, data);
