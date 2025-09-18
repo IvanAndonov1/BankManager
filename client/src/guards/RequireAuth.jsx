0439e8dcf1d9b289b381e48990224781317e4097
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-18 19:24:43 +0300

    authenticated users route guard

---
+++ client/src/guards/RequireAuth.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthContext";
+
+export default function RequireAuth() {
+	const { user } = useContext(AuthContext);
+	const loc = useLocation();
+
+	if (!user?.id) {
+		return <Navigate to="/login" replace state={{ from: loc }} />;
+	}
+
+	return <Outlet />;
+}
