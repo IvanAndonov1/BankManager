b3cec8bc17131faff76d10729414da93bf3627e3
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-19 11:12:09 +0300

    registration customer service

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -4,7 +4,7 @@ const baseUrl = 'http://localhost:8080/api';
 
 export const loginUser = (data) => requester.post(`${baseUrl}/auth/login`, data);
 
-export const registerUser = (data) => requester.post(`${baseUrl}/auth/register`, data);
+export const registerUser = (data) => requester.post(`${baseUrl}/auth/register/customer`, data);
 
 export const logoutUser = (token) => requester.post(`${baseUrl}/auth/logout`, {}, { 'Authorization': `Bearer ${token}` });
 
