36eaf3d6335693972bc70bf8fd3308e2230b362e
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-19 11:53:21 +0300

    render customers data in employee table

--- client/src/components/Employees/Dashboard/EmployeeTableRow.jsx
+++ client/src/components/Employees/Dashboard/EmployeeTableRow.jsx
@@ -4,8 +4,8 @@ function EmployeeTableRow(props) {
 	return (
 		<tr className="hover:bg-gray-50">
 			<td className="py-3">{props.name}</td>
-			<td className="py-3">{props.date}</td>
-			<td className="py-3 text-green-600">{props.status}</td>
+			<td className="py-3">{props.created_at.substr(0, 10)}</td>
+			<td className="py-3 text-green-600">{props.active ? 'active' : 'disabled'}</td>
 			<td className="py-3">
 				<Link to="/customer-details/1" className="cursor-pointer px-4 py-1 rounded-full text-white" style={{ backgroundColor: "#351f78" }}>
 					Show more
