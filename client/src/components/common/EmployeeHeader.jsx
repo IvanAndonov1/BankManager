1b7cb941cddf2ea13803f66bcbcf8c9caa7e15c9
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-19 12:00:18 +0300

    employee logout

--- client/src/components/common/EmployeeHeader.jsx
+++ client/src/components/common/EmployeeHeader.jsx
@@ -1,5 +1,6 @@
 import { use } from "react";
 import { AuthContext } from '../../contexts/AuthContext';
+import { Link } from "react-router";
 
 function EmployeeHeader() {
 	const { user } = use(AuthContext);
@@ -10,11 +11,11 @@ function EmployeeHeader() {
 				<p className="text-sm text-gray-600">
 					Logged as <span className="font-medium">{user.username}</span>
 				</p>
-				<button className="cursor-pointer p-2 rounded-lg border border-gray-300 text-gray-600 hover:bg-gray-50">
+				<Link to="/logout" className="cursor-pointer p-2 rounded-lg border border-gray-300 text-gray-600 hover:bg-gray-50">
 					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
 						<path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
 					</svg>
-				</button>
+				</Link>
 			</div>
 			<div className="bg-white rounded-lg shadow p-0 w-full">
 				<button
