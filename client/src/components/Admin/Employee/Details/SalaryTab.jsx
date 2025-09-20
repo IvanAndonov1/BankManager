9dbae6865f5b4da0618c781eb8cad0e6af77717d
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-20 17:25:43 +0300

    admin salary tab design changes

--- client/src/components/Admin/Employee/Details/SalaryTab.jsx
+++ client/src/components/Admin/Employee/Details/SalaryTab.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
+import { Link } from "react-router";
 
 export default function SalaryTab({ value = 1500 }) {
 	const [salary, setSalary] = useState(value);
 	return (
 		<div className="p-10 flex flex-col items-center gap-10">
 			<div className="flex items-center justify-center gap-16 mt-10">
-				<button type="button" aria-label="decrease" onClick={() => setSalary((s) => Math.max(0, s - 50))} className="w-12 h-12 rounded-full ring-2 ring-[#351F78] text-[#351F78] text-3xl flex items-center justify-center">−</button>
+				<button type="button" aria-label="decrease" onClick={() => setSalary((s) => Math.max(0, s - 50))} className="cursor-pointer w-12 h-12 rounded-full ring-2 ring-[#351F78] text-[#351F78] text-3xl flex items-center justify-center">−</button>
 				<div className="text-3xl">{salary} EUR</div>
-				<button type="button" aria-label="increase" onClick={() => setSalary((s) => s + 50)} className="w-12 h-12 rounded-full ring-2 ring-[#351F78] text-[#351F78] text-3xl flex items-center justify-center">+</button>
+				<button type="button" aria-label="increase" onClick={() => setSalary((s) => s + 50)} className="cursor-pointer w-12 h-12 rounded-full ring-2 ring-[#351F78] text-[#351F78] text-3xl flex items-center justify-center">+</button>
 			</div>
-			<button type="button" className="px-8 h-11 rounded-full text-white font-medium shadow bg-gradient-to-r from-[#351F78] via-[#3a4fb6] to-[#0b84b9] hover:opacity-95">Update</button>
+			<Link className="px-8 p-2 cursor-pointer h-11 rounded-full text-white font-medium shadow bg-gradient-to-r from-[#351F78] via-[#3a4fb6] to-[#0b84b9] hover:opacity-95">Update</Link>
 		</div>
 	);
 } 
