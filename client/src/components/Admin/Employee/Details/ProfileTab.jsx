faaca0e3b0945641e610848ccd86a246f9a1097b
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-22 12:28:38 +0300

    fill inputs with loaded data to profile employee tab

--- client/src/components/Admin/Employee/Details/ProfileTab.jsx
+++ client/src/components/Admin/Employee/Details/ProfileTab.jsx
@@ -1,13 +1,44 @@
+import { useState } from "react";
 import { Link } from "react-router";
 
-export default function ProfileTab() {
+export default function ProfileTab({ employee }) {
+	const [data, setData] = useState(employee);
+
 	return (
 		<form className="space-y-6 p-6">
 			<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-				<label className="block"><span className="block text-sm text-gray-600 mb-1">Full Name</span><input type="text" className="w-full h-10 rounded-full border border-gray-200 px-4 text-sm" /></label>
-				<label className="block"><span className="block text-sm text-gray-600 mb-1">Date of Birth</span><input type="text" className="w-full h-10 rounded-full border border-gray-200 px-4 text-sm" /></label>
-				<label className="block"><span className="block text-sm text-gray-600 mb-1">Email Address</span><input type="text" className="w-full h-10 rounded-full border border-gray-200 px-4 text-sm" defaultValue="example1@gmail.com" /></label>
-				<label className="block"><span className="block text-sm text-gray-600 mb-1">Position</span><input type="text" className="w-full h-10 rounded-full border border-gray-200 px-4 text-sm" defaultValue="Loan Officer" /></label>
+				<label className="block">
+					<span className="block text-sm text-gray-600 mb-1">Full Name</span>
+					<input
+						type="text"
+						className="w-full h-10 rounded-full border border-gray-200 px-4 text-sm"
+						defaultValue={`${data.firstName} ${data.lastName}`}
+					/>
+				</label>
+				<label className="block">
+					<span className="block text-sm text-gray-600 mb-1">Date of Birth</span>
+					<input
+						type="text"
+						className="w-full h-10 rounded-full border border-gray-200 px-4 text-sm"
+						defaultValue={data.dateOfBirth}
+					/>
+				</label>
+				<label className="block">
+					<span className="block text-sm text-gray-600 mb-1">Email Address</span>
+					<input
+						type="text"
+						defaultValue={data.email}
+						className="w-full h-10 rounded-full border border-gray-200 px-4 text-sm"
+					/>
+				</label>
+				<label className="block">
+					<span className="block text-sm text-gray-600 mb-1">Position</span>
+					<input
+						type="text"
+						className="w-full h-10 rounded-full border border-gray-200 px-4 text-sm"
+						defaultValue={data.position}
+					/>
+				</label>
 			</div>
 			<Link className="p-3 cursor-pointer px-8 h-11 rounded-full text-white font-medium shadow bg-gradient-to-r from-[#351F78] via-[#3a4fb6] to-[#0b84b9] hover:opacity-95">Update</Link>
 		</form>
