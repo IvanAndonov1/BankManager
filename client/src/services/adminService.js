bdfd8fe0a6b30b0ea639b1eaded648eb06be166e
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-22 12:30:03 +0300

    admin get employee by id service

--- client/src/services/adminService.js
+++ client/src/services/adminService.js
@@ -3,3 +3,5 @@ import requester from './requester';
 const baseUrl = 'http://localhost:8080/api';
 
 export const getAllEmployees = (token) => requester.get(`${baseUrl}/employees/all`, { 'Authorization': `Bearer ${token}` });
+
+export const getEmployeeById = (employeeId, token) => requester.get(`${baseUrl}/employees/by-id/${employeeId}`, { 'Authorization': `Bearer ${token}` });
