9980c1adeb678fdb9597799e0566aab9ca9cb59b
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-23 14:06:25 +0300

    icon component

---
+++ client/src/components/Employees/Customer/Details Subcomponents/XIIcon.jsx
@@ -0,0 +1,9 @@
+function XIcon(props) {
+	return (
+		<svg viewBox="0 0 24 24" className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="2" {...props}>
+			<path d="M6 6l12 12M18 6L6 18" strokeLinecap="round" strokeLinejoin="round" />
+		</svg>
+	);
+}
+
+export default XIcon;
