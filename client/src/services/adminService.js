3441f792c9482755b9977ba6d26925c6c90ef113
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-09-26 01:23:42 +0300

    admin api service functions

--- client/src/services/adminService.js
+++ client/src/services/adminService.js
@@ -5,3 +5,9 @@ const baseUrl = 'http://localhost:8080/api';
 export const getAllEmployees = (token) => requester.get(`${baseUrl}/employees/all`, { 'Authorization': `Bearer ${token}` });
 
 export const getEmployeeById = (employeeId, token) => requester.get(`${baseUrl}/employees/by-id/${employeeId}`, { 'Authorization': `Bearer ${token}` });
+
+export const editEmployeeInformation = (employeeId, token, data) => requester.put(`${baseUrl}/employees/${employeeId}`, { 'Authorization': `Bearer ${token}` }, data);
+
+export const promoteEmployee = (employeeId, token) => requester.put(`${baseUrl}/employees/${employeeId}/promote`, { 'Authorization': `Bearer ${token}` }, {});
+
+export const fireEmployee = (employeeId, token) => requester.put(`${baseUrl}/employees/${employeeId}/fire`, { 'Authorization': `Bearer ${token}` }, {});
